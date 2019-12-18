10141dd7d6a3c1789e49af8fe6f1abcc45bfffc2
scottyboi30 <49397999+scottyboi30@users.noreply.github.com>
2019-12-18 23:29:09 +0000

    Deploying to gh-pages from master 79a5f7f6b0a0f8035c1f92dec0329168ac203804

---
+++ precache-manifest.f3be25516108bce465b15067262b8bc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fbe39720f996c52b922617f3454828",
+    "url": "/scotty-clothing/index.html"
+  },
+  {
+    "revision": "de1ef5e2b580e584c36b",
+    "url": "/scotty-clothing/static/css/main.15c50361.chunk.css"
+  },
+  {
+    "revision": "286600dd3d581f042977",
+    "url": "/scotty-clothing/static/js/2.b9aa6595.chunk.js"
+  },
+  {
+    "revision": "de1ef5e2b580e584c36b",
+    "url": "/scotty-clothing/static/js/main.86efeee3.chunk.js"
+  },
+  {
+    "revision": "7c80ffff0e8a696cbb5f",
+    "url": "/scotty-clothing/static/js/runtime~main.e551b6db.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/scotty-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/scotty-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
